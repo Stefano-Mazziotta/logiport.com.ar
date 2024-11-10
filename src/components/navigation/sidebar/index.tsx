2b782269273dd1dbdd8ee75126e74a4da607d418
stefano-mazziotta <mazziottastefano@gmail.com>
2024-11-10 00:58:59 -0300

    improve navigation

--- src/components/navigation/sidebar/index.tsx
+++ src/components/navigation/sidebar/index.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import { ITEMS_NAVIGATION } from "@/placeholder-data";
 import { useContext } from "react";
+import { ITEMS_NAVIGATION } from "@/placeholder-data";
 import { NavigationContext } from "@/context/navigation-context";
 
 const Sidebar = (): JSX.Element => {
@@ -8,27 +8,24 @@ const Sidebar = (): JSX.Element => {
   return (
     <>
       <div
-        className={`sidebar-container fixed w-full h-full overflow-hidden justify-center bg-primary text-white grid pt-[120px] left-0 z-10 transition-all duration-500 ease-in-out ${
+        className={`fixed w-full h-full overflow-hidden justify-center bg-primary text-white grid pt-[120px] left-0 z-10 transition-all duration-500 ease-in-out ${
           isOpenSidebar ? "opacity-100 top-0" : "opacity-0 -top-full"
         }`}
       >
-        <ul className="sidebar-nav text-center leading-relaxed text-xl">
-          <li>
-            <Link href="#about" onClick={handleCloseSidebar}>
-              <p>About Us</p>
-            </Link>
-          </li>
-          <li>
-            <Link href="#services" onClick={handleCloseSidebar}>
-              <p>Services</p>
-            </Link>
-          </li>
-          <li>
-            <Link href="#contacts" onClick={handleCloseSidebar}>
-              <p>Contacts</p>
-            </Link>
-          </li>
-        </ul>
+        <div className="flex flex-col gap-10 items-center text-2xl justify-between p-10">
+          <ul className="flex flex-col gap-3 text-center leading-relaxed">
+            {ITEMS_NAVIGATION.map((item, index) => {
+              return (
+                <li key={index}>
+                  <Link href={item.href} onClick={handleCloseSidebar}>
+                    <p>{item.text}</p>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+          <h1 className="font-extrabold">LOGIPORT S.R.L.</h1>
+        </div>
       </div>
     </>
   );
