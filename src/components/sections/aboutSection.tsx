45c86d9be6a597760a2468d8b882f4dfa216abae
stefano-mazziotta <mazziottastefano@gmail.com>
2024-12-03 23:46:51 -0300

    working on about us

--- src/components/sections/aboutSection.tsx
+++ src/components/sections/aboutSection.tsx
@@ -4,7 +4,7 @@ import { ChevronRight } from "lucide-react";
 
 export default function AboutSection() {
   return (
-    <section className="relative w-full max-w-6xl mx-auto px-4">
+    <section id="about" className="relative w-full max-w-6xl mx-auto px-4">
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-2 items-center">
         <div className="relative w-full h-[300px] overflow-hidden shadow-md p-10 border-solid border-foreground lg:h-[600px] rounded-md">
           <div className="absolute inset-0 bg-black opacity-30 z-10"></div>
@@ -17,28 +17,29 @@ export default function AboutSection() {
           />
         </div>
         <div className="relative">
-          <div className="bg-[#1a1332] z-20 text-white p-8 rounded-md lg:absolute lg:-left-24 lg:top-1/2 lg:-translate-y-1/2 lg:w-[120%] shadow-2xl">
-            <h2 className="text-3xl font-bold mb-6 flex items-center">
-              <ChevronRight />
-              About us
+          <div className="bg-[#1a1332] text-secondary z-20 p-8 rounded-md lg:absolute lg:-left-24 lg:top-1/2 lg:-translate-y-1/2 lg:w-[120%] shadow-2xl">
+            <h2 className="text-2xl font-extrabold mb-6 flex items-center md:text-4xl">
+              <ChevronRight size={36} className="text-primary" />
+              Sobre nosotros
             </h2>
             <div className="space-y-4">
-              <p className="text-gray-200">
-                For more than 30 years we have been delivering world-class
-                construction and we&apos;ve built many lasting relationships
-                along the way.
+              <p>
+                LOGIPORT S.R.L. fundada en junio de 2018 y registrada ante
+                Prefectura Naval Argentina, responde a la necesidad de asesorar
+                a la comunidad portuaria en protección y gestión de
+                instalaciones.
               </p>
-              <p className="text-gray-200">
-                We&apos;ve matured into an industry leader and trusted resource
-                for those seeking quality, innovation and reliability when
-                building in the U.S.
+              <p>
+                Especializados en formación y capacitación según el Código PBIP,
+                contamos con amplia experiencia en la protección de buques e
+                instalaciones portuarias.
               </p>
             </div>
             <Button
               className="mt-8 bg-white text-[#1a1332] hover:bg-gray-100"
               size="lg"
             >
-              More on Our History
+              Contáctanos
             </Button>
           </div>
         </div>
